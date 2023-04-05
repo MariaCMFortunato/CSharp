efcc2e4dc17481744b85e8e39b6bd9ffdb7c66ab
Maria <conceicao.martinho.estudo@gmail.com>
2023-04-05 23:55:56 +0100

    05/04/2023
    Novos projetos C#

--- ConversorTemperatura/Conversor.cs
+++ ConversorTemperatura/Conversor.cs
@@ -2,3 +2,4 @@ int fahrenheit = 94;
 decimal degrees;
 degrees = (decimal) (fahrenheit - 32m) * (5m / 9m);
 Console.WriteLine($"The temperature in degrees is:  {degrees} celsius");
+Console.WriteLine(5 / 10);
