6ee0334159d857e751867c28e4c994a8d133948e
Maria <conceicao.martinho.estudo@gmail.com>
2023-04-12 01:27:46 +0100

    12/04/2023

---
+++ TestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//Uso do método Random.Next() para simular o lançamento de um dado
+Random dice = new Random();
+int roll = dice.Next(1,7);
+Console.WriteLine(roll);
+
+/*int result = Random.Next();
+Console.WriteLine(result);
+Dá erro pois tem de ser criado um ojeto para aceder ao método Random.Next()*/
+Random resultado = new Random();
+int numero = resultado.Next();
+Console.WriteLine($"\n{numero}");
