7334444f4925010b7a9e22b4fb1c1d263413a02d
Maria <conceicao.martinho.estudo@gmail.com>
2025-02-24 17:51:20 +0000

    Atualização
    Atualização 20250204

--- TesteCSharp/obj/Debug/net7.0/CSharp.AssemblyInfo.cs
+++ TesteCSharp/obj/Debug/net7.0/CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d3b772d7f4f8787ebed53535dbc544153ee8b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12675ac921197188b5c284f963dd373d894f51ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
